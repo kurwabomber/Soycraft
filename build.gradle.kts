8054110ba7ea3147efb6ebf2b100b67b2a3be221
David Nguyen <davidWnguyen@proton.me>
2023-11-08 18:47:20 -0500

    HELO WRLD

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "net.razorclan"
+version = "1.0-INDEV"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
